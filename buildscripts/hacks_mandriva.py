52001d0ac4e83d71cdae37fcf7882ecd24535022
Ludovic Bellière <xrogaan@gmail.com>
2010-03-22 18:10:29 +0100

    Append the header path to CPPPATH

--- buildscripts/hacks_mandriva.py
+++ buildscripts/hacks_mandriva.py
@@ -3,6 +3,4 @@ import os
 
 def insert( env , options ):
     if os.path.exists( "/usr/include/js-1.70/" ):
-        env.Append( CPPDEFINES=[ "LIBJS" ] )
-        print('Append LIBJS')
-
+        env.Append( CPPPATH=[ "/usr/include/js-1.70/" ] )
